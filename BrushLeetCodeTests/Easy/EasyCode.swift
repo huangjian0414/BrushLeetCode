a45664a96a23fd4d9b66bf455804738b1e62114e
huangjian <huangjian@lanyife.com>
2019-06-13 18:43:39 +0800

    整数反转（32位限制），判断整数是否为回文数， 罗马数字转整数

--- BrushLeetCodeTests/Easy/EasyCode.swift
+++ BrushLeetCodeTests/Easy/EasyCode.swift
@@ -23,4 +23,56 @@ struct EasyCode {
         
         return [];
     }
+    //MARK: - 整数反转（假设我们的环境只能存储得下 32 位的有符号整数，则其数值范围为 [−2^31,  2^31 − 1]）
+    static func reverseInt(_ x: Int) -> Int{
+        var reverse = 0
+        var value = x   //如 3576
+        while value != 0 {//取余，一步步把最后的数移到前面
+            let pop = value % 10 //6 -- 7 -- 5 -- 3
+            value = value/10     //357 -- 35 -- 3 -- 0
+            reverse = reverse * 10 + pop //6 -- 67 -- 675 -- 6753
+            if reverse > Int32.max || (reverse == Int32.max && pop > 7) || reverse < Int32.min || (reverse == Int32.min && pop < -8) { return 0}//超过存储范围溢出
+        }
+        return reverse;
+    }
+    //MARK: - 判断整数是否为回文数 (回文数：正反都一样)
+    static func isPalindrome(_ x: Int) -> Bool {
+        if x < 0 || (x % 10 == 0 && x != 0) {//x小于0不是回文数，x最后一位是0，只有x=0才是回文数
+            return false
+        }
+        var reverse = 0
+        var value = x   //如 12321
+        while value > reverse { //反转一半
+            reverse = reverse * 10 + value % 10  //1 -- 12 -- 123
+            value = value / 10 //1232 -- 123 -- 12
+        }
+        return value == reverse || value == reverse / 10
+    }
+    //MARK: - 罗马数字转整数
+    static func romanToInt(_ s: String) -> Int {
+        var sum = 0
+        let map = ["I":1,"V":5,"X":10,"L":50,"C":100,"D":500,"M":1000]
+        
+        /*
+         如M CM XC IV = M + (M-C) + (C-X) + (V-I)  : M=1000 CM=M-C=900 XC=C-X=90 IV=V-I=4
+         i=0  C<M  sum = M
+         i=1  M>C  sum = M - C
+         i=2  X<M  sum = M - C + M
+         i=3  C>X  sum = M - C + M - X
+         i=4  I<C  sum = M - C + M - X + C
+         i=5  V>I  sum = M - C + M - X + C - I
+         i=6       sum = M - C + M - X + C - I + V = 1000-100+1000-10+100-1+5 = 1994
+         */
+        
+        for (i,value) in s.enumerated() {
+            if i == s.count - 1 ||
+                map[String(s[s.index(s.startIndex, offsetBy: i + 1)..<s.index(s.startIndex, offsetBy: i + 2)])]!
+                <= map[String(value)]!{//如果下一个字节从字典里取到的值小于等于当前字节从字典里取到的值，加上当前的值
+                sum += map[String(value)]!
+            } else { //反之 减去当前的值
+                sum -= map[String(value)]!
+            }
+        }
+        return sum
+    }
 }
--- BrushLeetCodeTests/Easy/EasyManager.swift
+++ BrushLeetCodeTests/Easy/EasyManager.swift
@@ -14,6 +14,27 @@ class EasyManager: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
         //twoSum()
+        //reverseInt()
+        //isPalindrome()
+        //romanToInt()
+    }
+    //MARK: - 罗马数字转整数
+    func romanToInt() {
+        let value = "MCMXCIV"
+        let num = EasyCode.romanToInt(value)
+        print(num)
+    }
+    //MARK: - 判断整数是否为回文数
+    func isPalindrome() {
+        let value = 12321
+        let isPalindrome = EasyCode.isPalindrome(value)
+        print(isPalindrome)
+    }
+    //MARK: - 整数反转
+    func reverseInt() {
+        let num = 7463847412
+        let reverseNum = EasyCode.reverseInt(num)
+        print(reverseNum)
     }
     //MARK: - 两数之和
     func twoSum() {
@@ -22,11 +43,7 @@ class EasyManager: XCTestCase {
         let array = EasyCode.twoSum(nums, target)
         print(array)
     }
-    
-    
-    
-    
-    
+
     
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
